75df124710438cd12ff005e4e9732ec20a5d7578
Elvis Carrasco <elvissamir18@gmail.com>
2022-10-04 18:51:00 -0400

    Add icons to footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,12 +1,41 @@
+import CogIcon from "../../assets/icons/CogIcon"
+import GithubIcon from "../../assets/icons/GithubIcon"
+import LinkedinIcon from "../../assets/icons/LinkedinIcon"
+import MailIcon from "../../assets/icons/MailIcon"
+import WorldIcon from "../../assets/icons/WorldIcon"
+
 const Footer = () => {
+    const externalNetworks = [
+        { url: '/', icon: <WorldIcon /> },
+        { url: 'https://github.com/Elvissamir', icon: <GithubIcon /> },
+        { url: 'https://linkedin.com/in/elvissam', icon: <LinkedinIcon /> }
+    ]
+
     return (
         <div className="footer-container">
             <footer className="footer">
-                <p>My Social Networks:</p>
-                <p>elvissam.software@gmail.com</p>
-                <p>Designed and Developed by Elvis Carrasco</p>
-                <p>AlgoUI - Algorithms And Data Structures</p>
-                <p>2022</p>
+                <div className="my-networks">
+                    <p>My Social Networks:</p>
+                    <div className="my-email">
+                        <MailIcon />
+                        <p>elvissam.software@gmail.com</p>
+                    </div>
+                    <div className="other-networks">
+                        {externalNetworks.map((network, index) => 
+                            <a key={index} href={network.url} className="external-link-icon">
+                                {network.icon}
+                            </a>
+                        )}
+                    </div>
+                </div>
+                <div className="developed-by">
+                    <CogIcon />
+                    <p>Designed and Developed by Elvis Carrasco</p>
+                </div>
+                <div className="footer-bottom">
+                    <p>AlgoUI - Algorithms And Data Structures</p>
+                    <p>2022</p>
+                </div>
             </footer>
         </div>
     )

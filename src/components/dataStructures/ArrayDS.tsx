90b978c7ffea3808228fe45273a9cd3afe13b94d
Elvis Carrasco <elvissamir18@gmail.com>
2022-10-15 11:40:53 -0400

    Add controlled animation for add item to Array Data Structure

--- src/components/dataStructures/ArrayDS.tsx
+++ src/components/dataStructures/ArrayDS.tsx
@@ -1,30 +1,108 @@
 import { useEffect, useState } from 'react'
-import { motion, AnimatePresence, useAnimationControls } from 'framer-motion'
-import { useTransition, animated } from "@react-spring/web";
+import { motion, AnimatePresence, Variants, useAnimationControls } from 'framer-motion'
 
 type ArrayOperation = 'add-start' | 'add-end' | 'remove-start' | 'remove-end' | null
 
 const ArrayDS = () => {
-    const [dataArray, setDataArray] = useState([1, 2, 3, 4, 5])
-    const [newItem, setNewItem] = useState<number | null>(23)
+    const [dataArray, setDataArray] = useState([
+        { id: Math.random(), val: 1 },
+        { id: Math.random(), val: 2 },
+        { id: Math.random(), val: 3 },
+        { id: Math.random(), val: 4 },
+        { id: Math.random(), val: 5 }
+    ])
+
+    const arrayItemVariants: Variants = {
+        show: {
+            opacity: [0, 1]
+        },
+        hidden: {
+            opacity: 0
+        },
+        new: { 
+            opacity: [0, 1],
+        },
+        removed: {
+            opacity: 0
+        },
+        exit: {
+            opacity: 0
+        }
+    }
+
+    const listVariants = {
+        hidden: { opacity: 0 },
+        show: {
+          opacity: 1,
+          transition:{
+            type: 'spring',
+            staggerChildren: 0.5
+          }
+        }
+      }
+
+    const item = {
+        show: {
+            opacity: 1
+        },
+        hidden: {
+            opacity: 0
+        },
+        new: { 
+            opacity: [0, 1],
+        },
+        removed: {
+            opacity: 0
+        }
+    }
+
     const [operation, setOperation] = useState<ArrayOperation>(null)
+    const [nid, setnId] = useState(0)
+    const [nindex, setnIndex] = useState(0)
+
+    const controls = useAnimationControls()
+
+    useEffect(() => {
+        if (operation === null) {
+            controls.start(i => ({
+                opacity: [0, 1],
+                x: [-50, 0],
+                transition: { delay: i * 0.5 },
+             }))
+        }
+
+        if (operation === 'add-start') {
+            controls.start(i => ({
+               color: i === 0? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
+               backgroundColor: i === 0? ['#312e81', '#312e81', '#312e81', '#ffff'] : 'transparent',
+               opacity: i === 0? [1, 1, 1, 1] : 1,
+               x: i === 0? [-50, 0] : [5, 0],
+               transition: { delay: i === 0? 0.05 : i * 0.025 },
+            }))
+        }
+
+        if (operation === 'remove-start') controls.start('hidden')
+    }, [ dataArray ])
 
     const handleAddToStart = () => {
-        if (newItem) {
-            const ndataArray = [...dataArray]
-            ndataArray.unshift(newItem)
-            setDataArray(ndataArray)
-        }
+        const ndataArray = [...dataArray]
+        const nid = Math.floor(Math.random() * 10)
+    
+        setnId(nid)
+        setnIndex(0)
+
+        ndataArray.unshift({ id: Math.random(), val: nid})
+        setDataArray(ndataArray)
 
         setOperation('add-start')
     }
 
     const handleAddToEnd = () => {
-        if (newItem) {
-            const ndataArray = [...dataArray]
-            ndataArray.push(newItem)
-            setDataArray(ndataArray)
-        }
+        const ndataArray = [...dataArray]
+        ndataArray.push({ id: Math.random(), val: Math.floor(Math.random() * 10) })
+        setDataArray(ndataArray)
+
+        setOperation('add-end')
     }
 
     const handleRemoveFromStart = () => {
@@ -67,8 +145,8 @@ const ArrayDS = () => {
                             <input className="input-text" id='index-val' type="text" />
                         </div>
                         <button type="button">Remove From</button>
-                        <button onClick={handleRemoveFromStart} type="button">Remove Last</button>
-                        <button onClick={handleRemoveFromEnd} type="button">Remove First</button>
+                        <button onClick={handleRemoveFromStart} type="button">Remove First</button>
+                        <button onClick={handleRemoveFromEnd} type="button">Remove Last</button>
                     </div>
                     <div className="sort-items-controls">
                         <button type="button">Sort (increasing)</button>
@@ -104,7 +182,22 @@ const ArrayDS = () => {
                     </div>
                 </div>
                 <div className='section-action'>
-                    <div className='array-container'></div>
+                    <div className='array-container'>
+                        <ul className='array-item-list'>
+                            <AnimatePresence>     
+                                {dataArray.map((item, index) => 
+                                        <motion.li 
+                                            className='array-item' 
+                                            variants={arrayItemVariants}
+                                            animate={controls}
+                                            custom={index}
+                                            key={item.id}>
+                                                {item.val}
+                                        </motion.li> 
+                                    )}
+                            </AnimatePresence>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className="section-bottom"></div>

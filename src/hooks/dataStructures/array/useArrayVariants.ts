15584fbeebc6f687696f45afd1b9bc6f845200b9
Elvis Carrasco <elvissamir18@gmail.com>
2022-10-19 18:57:41 -0400

    Extract Array variants

---
+++ src/hooks/dataStructures/array/useArrayVariants.ts
@@ -0,0 +1,156 @@
+import { Transition } from "framer-motion"
+import { ArrayItem } from "../../../core/dataStructures/ArrayDS"
+
+interface UseArrayVariantsProps {
+    dataArray: ArrayItem[]
+    actionIndex: number
+}
+
+const useArrayVariants = ({ dataArray, actionIndex }: UseArrayVariantsProps) => {
+    const displayArrayVariants = () => {
+        return (i: number) => ({
+            opacity: [0, 1],
+            x: [-50, 0],
+            transition: { delay: i * 0.5 },
+        })
+    }
+
+    const addToStartVariants = () => {
+        return (i: number) => ({
+            color: i === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
+            backgroundColor: i === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff',
+            opacity: i === actionIndex? [0, 1, 1, 1] : 1,
+            x: i === actionIndex? [-50, 0] : 0,
+            transition: { delay: i === actionIndex? 0.05 : i * 0.025, bounce: 0 },
+        })
+    }
+
+    const addToPositionVariants = () => {
+        const color = (index: number) => {
+            return index === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000'
+        }
+
+        const backgroundColor = (index: number): string | string[] => {
+            return index === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff'
+        }
+
+        const opacity = (index: number): number | number[] => {
+            return index === actionIndex? 1 : 1
+        }
+
+        const y = (index: number): number | number[] => {
+            if (index < actionIndex) return 0
+
+            if (index > actionIndex) return 0
+
+            return [70, 0]
+        }
+
+        const transition = (index: number): Transition => {
+            return { delay: index === 0? 0.05 : index * 0.025 }
+        }
+
+        return (i: number) => ({
+            color: color(i),
+            backgroundColor: backgroundColor(i),
+            opacity: opacity(i),
+            y: y(i),
+            transition: transition(i),
+        })
+    }
+
+    const removeFromPositionVariants = () => {
+        const color = (index: number) => {
+            return index === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000'
+        }
+
+        const backgroundColor = (index: number): string | string[] => {
+            return index === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff'
+        }
+
+        const opacity = (index: number): number | number[] => {
+            return index === actionIndex? [1, 1, 0, 1, 0] : 1
+        }
+
+        const y = (index: number): number | number[] => {
+            if (index === actionIndex) return [0, 70]
+
+            else return 0
+        }
+
+        const transition = (index: number): Transition => {
+            return { delay: index === actionIndex? 0.05 : index * 0.025 }
+        }
+
+        return (i: number) => ({
+            color: color(i),
+            backgroundColor: backgroundColor(i),
+            opacity: opacity(i),
+            y: y(i),
+            transition: transition(i),
+        })
+    }
+
+    const removeFromStartVariants = () => {
+        return (i: number) => ({
+            color: i === 0? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
+            backgroundColor: i === 0? '#312e81' : '#ffff',
+            opacity: i === 0? [1,0,1,0] : 1,
+            position: i === 0? 'absolute' : 'static',
+            x: i === 0? -50 : 0,
+            transition: { delay: i === 0? 0.05 : i * 0.025 },
+        })
+    }
+
+    const removeFromEndVariants = () => {
+        return (i: number) => ({
+            color: i === dataArray.length - 1? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
+            backgroundColor: i === dataArray.length - 1? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff',
+            opacity: i === dataArray.length - 1? [0, 1, 0, 1] : 1,
+            x: i === dataArray.length - 1? 50 : 0,
+            transition: { delay: i === dataArray.length - 1? 0.05 : i * 0.025 },
+        })
+    }
+
+    const multiplyByVariants = () => {
+        return (i: number) => ({
+            opacity: i === actionIndex ? 1 : 1,
+            backgroundColor: i === actionIndex? ['#312e81', '#fff'] : '#fff',
+            color: i === actionIndex? [ '#fff', '#000000'] : '#000000',
+            transition: { duration: 0.2 }
+        })
+    }
+
+    const filterVariants = () => {
+        return (i: number) => ({
+            opacity: i === actionIndex ? 1 : 1,
+            backgroundColor: i === actionIndex? ['#312e81', '#fff'] : '#fff',
+            color: i === actionIndex? [ '#fff', '#000000'] : '#000000',
+            transition: { duration: 0.2 }
+        })
+    }
+    
+    const addToEndVariants = () => {
+        return (i: number) => ({
+            color: i === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
+            backgroundColor: i === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff',
+            opacity: i === actionIndex? [0, 1] : 1,
+            x: i === actionIndex? [50, 50,  0] : 0,
+            transition: { delay: i === actionIndex? 0.05 : i * 0.025 }
+        })
+    }
+
+    return {
+        displayArrayVariants,
+        addToStartVariants,
+        addToEndVariants,
+        addToPositionVariants,
+        removeFromStartVariants,
+        removeFromPositionVariants,
+        removeFromEndVariants,
+        multiplyByVariants,
+        filterVariants,
+    }
+}
+
+export default useArrayVariants
--- src/pages/dataStructures/ArrayDSPage.tsx
+++ src/pages/dataStructures/ArrayDSPage.tsx
@@ -13,117 +13,6 @@ const ArrayDSPage = () => {
 
     const controls = useAnimationControls()
 
-    const displayArrayVariants = () => {
-        return (i: number) => ({
-            opacity: [0, 1],
-            x: [-50, 0],
-            transition: { delay: i * 0.5 },
-        })
-    }
-
-    const addItemToStartVariants = () => {
-        return (i: number) => ({
-            color: i === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
-            backgroundColor: i === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff',
-            opacity: i === actionIndex? [0, 1, 1, 1] : 1,
-            x: i === actionIndex? [-50, 0] : 0,
-            transition: { delay: i === actionIndex? 0.05 : i * 0.025, bounce: 0 },
-        })
-    }
-
-    const addItemToPositionVariants = () => {
-        const color = (index: number) => {
-            return index === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000'
-        }
-
-        const backgroundColor = (index: number): string | string[] => {
-            return index === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff'
-        }
-
-        const opacity = (index: number): number | number[] => {
-            return index === actionIndex? 1 : 1
-        }
-
-        const y = (index: number): number | number[] => {
-            if (index < actionIndex) return 0
-
-            if (index > actionIndex) return 0
-
-            return [70, 0]
-        }
-
-        const transition = (index: number): Transition => {
-            return { delay: index === 0? 0.05 : index * 0.025 }
-        }
-
-        return (i: number) => ({
-            color: color(i),
-            backgroundColor: backgroundColor(i),
-            opacity: opacity(i),
-            y: y(i),
-            transition: transition(i),
-        })
-    }
-
-    const removeItemFromPositionVariants = () => {
-        const color = (index: number) => {
-            return index === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000'
-        }
-
-        const backgroundColor = (index: number): string | string[] => {
-            return index === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff'
-        }
-
-        const opacity = (index: number): number | number[] => {
-            return index === actionIndex? [1, 1, 0, 1, 0] : 1
-        }
-
-        const y = (index: number): number | number[] => {
-            if (index === actionIndex) return [0, 70]
-
-            else return 0
-        }
-
-        const transition = (index: number): Transition => {
-            return { delay: index === actionIndex? 0.05 : index * 0.025 }
-        }
-
-        return (i: number) => ({
-            color: color(i),
-            backgroundColor: backgroundColor(i),
-            opacity: opacity(i),
-            y: y(i),
-            transition: transition(i),
-        })
-    }
-
-    const multiplyByItemVariants = () => {
-        return (i: number) => ({
-            opacity: i === actionIndex ? 1 : 1,
-            backgroundColor: i === actionIndex? ['#312e81', '#fff'] : '#fff',
-            color: i === actionIndex? [ '#fff', '#000000'] : '#000000',
-            transition: { duration: 0.2 }
-        })
-    }
-
-    const filterItemVariants = () => {
-        return (i: number) => ({
-            opacity: i === actionIndex ? 1 : 1,
-            backgroundColor: i === actionIndex? ['#312e81', '#fff'] : '#fff',
-            color: i === actionIndex? [ '#fff', '#000000'] : '#000000',
-            transition: { duration: 0.2 }
-        })
-    }
-    
-    const addItemToEndVariants = () => {
-        return (i: number) => ({
-            color: i === actionIndex? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
-            backgroundColor: i === actionIndex? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff',
-            opacity: i === actionIndex? [0, 1] : 1,
-            x: i === actionIndex? [50, 50,  0] : 0,
-            transition: { delay: i === actionIndex? 0.05 : i * 0.025 }
-        })
-    }
 
     const displayArrayAction = async () => {
         await controls.start(displayArrayVariants())
@@ -285,14 +174,7 @@ const ArrayDSPage = () => {
     }
 
     const handleRemoveFromStart = async () => {
-        await controls.start(i => ({
-            color: i === 0? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
-            backgroundColor: i === 0? '#312e81' : '#ffff',
-            opacity: i === 0? [1,0,1,0] : 1,
-            position: i === 0? 'absolute' : 'static',
-            x: i === 0? -50 : 0,
-            transition: { delay: i === 0? 0.05 : i * 0.025 },
-        }))
+        await controls.start()
 
         setOperation('remove-start')
         handleRemoveFromStartAction()
@@ -306,13 +188,7 @@ const ArrayDSPage = () => {
     }   
 
     const handleRemoveFromEnd = async () => {
-        await controls.start(i => ({
-            color: i === dataArray.length - 1? ['#ffff', '#ffff', '#ffff', '#000000'] : '#000000',
-            backgroundColor: i === dataArray.length - 1? ['#312e81', '#312e81', '#312e81', '#ffff'] : '#ffff',
-            opacity: i === dataArray.length - 1? [0, 1, 0, 1] : 1,
-            x: i === dataArray.length - 1? 50 : 0,
-            transition: { delay: i === dataArray.length - 1? 0.05 : i * 0.025 },
-        }))
+        await controls.start()
 
         setOperation('remove-end')
         handleRemoveFromEndAction()

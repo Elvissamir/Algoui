707b7040c287b914ea29f1af365fdae642c4a431
Elvis Carrasco <elvissamir18@gmail.com>
2022-10-19 13:48:53 -0400

    Add SectionContainer component for content layout

---
+++ src/components/SectionContainer.tsx
@@ -0,0 +1,27 @@
+interface SectionContainerProps {
+    title: string
+    description: string
+    action: JSX.Element
+    controls: JSX.Element
+    bottom: JSX.Element
+}
+
+const SectionContainer = ({ title, description, action, controls, bottom }: SectionContainerProps) => {
+    return (
+        <section className="section-wrapper">
+            <div className="section-top">
+                <h1 className="section-title">{title}</h1>
+            </div>
+            <div className="section-info">
+                <p className="text-info">{description}</p>
+            </div>
+            <div className="section-action-wrapper">
+                <div className="section-controls">{controls}</div>
+                <div className='section-action'>{action}</div>
+            </div>
+            <div className="section-bottom">{bottom}</div>
+        </section>
+    )
+}
+
+export default SectionContainer

5c66064c7aa148c4760fe259ad0c63e0617da79d
Elvis Carrasco <elvissamir18@gmail.com>
2022-10-21 12:23:40 -0400

    Add InputPackField component

--- src/components/InputPackField.tsx
+++ src/components/InputPackField.tsx
@@ -1,17 +1,33 @@
+import { ChangeEvent } from "react"
+import { FieldError } from "../core/generalTypes"
+import FieldErrorInfo from "./FieldErrorInfo"
+import Input from "./Input"
 import InputCheck from "./InputCheck"
 import Label from "./Label"
 
-const InputPackField = () => {
+interface InputPackFieldProps {
+    inputId: string 
+    label : string 
+    inputValue: string
+    inputType: 'text' | 'number'
+    checkId: string
+    checkName: string 
+    checked: boolean
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void
+    error: FieldError
+}
+
+const InputPackField = ({ inputId, inputValue, inputType, label, checkId, checkName, checked, error, handleChange }: InputPackFieldProps) => {
     return (
         <div className="input-field-container">
             <div className="input-field">
-                <Label />
+                <Label text={label} inputId={inputId} />
                 <div className="input-pack-container">
-                    <Input />
-                    <InputCheck />
+                    <Input id={inputId} value={inputValue} type={inputType} handleChange={handleChange} />
+                    <InputCheck id={checkId} name={checkName} checked={checked} handleChange={handleChange} />
                 </div>
             </div>
-            { errors['high-val'] && <FieldErrorInfo error={errors['high-val']} />}
+            { error && <FieldErrorInfo error={error} />}
         </div>
     )
 }

993549794cad366ef2b3a7d8882c178ac18644ee
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-01 17:27:54 -0400

    Add behaviour to ControlsDropdown component

--- src/components/ControlsDropdown.tsx
+++ src/components/ControlsDropdown.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
 import { ControlsOption } from "./SectionContainer"
 
 interface ControlsDropdownProps {   
@@ -5,13 +7,33 @@ interface ControlsDropdownProps {
 }
 
 const ControlsDropdown = ({ options }: ControlsDropdownProps) => {
+    const [activeOptionIndex, setActiveOptionIndex] = useState<number | null>(null)
+
+    const handleToggleOption = (index: number) => {
+        if (index === activeOptionIndex) return setActiveOptionIndex(null)
+
+        setActiveOptionIndex(index)
+    }
+
     return (
         <div className="controls-dropdown-container">
             <ul className="controls-dropdown scroll">
                 {options.map((option, index) => 
                     <li className="dropdown-option" key={index}>
-                        <button className="dropdown-option-btn">{option.name}</button>
-                        <div className="dropdown-content">{option.content}</div>
+                        <button 
+                            onClick={() => handleToggleOption(index)}
+                            className="dropdown-option-btn">{option.name}</button>
+                        <AnimatePresence>
+                            {index === activeOptionIndex &&
+                                <motion.div 
+                                    initial={{ opacity: 0, height: 0 }}
+                                    animate={{ opacity: 1, height: 'auto' }}
+                                    exit={{ opacity: 0, height: 0 }}
+                                    transition={{ bounce: 0 }}
+                                    className="dropdown-content">
+                                        {option.content}
+                                </motion.div>}
+                        </AnimatePresence>
                     </li>
                 )}
             </ul>

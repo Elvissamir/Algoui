e147df632f0d4ee0a5a853b524b89fa6be311e55
Elvis Carrasco <elvissamir18@gmail.com>
2022-11-02 18:28:39 -0400

    Add new algoui logo component

---
+++ src/components/AlgoUILogo.tsx
@@ -0,0 +1,26 @@
+const AlgoUILogo = () => {
+    return (
+        <div className="algoui-logo-container">
+            <div className="row">
+                <div className="logo-circle circle-1"></div>
+                <div className="logo-circle circle-2"></div>
+                <div className="logo-circle circle-3"></div>
+            </div>
+            <div className="row">
+                <div className="logo-circle circle-4"></div>
+                <div className="logo-circle circle-5"></div>
+                <div className="logo-circle circle-6"></div>
+            </div>
+            <div className="row">
+                <div className="logo-circle circle-7"></div>
+                <div className="logo-circle circle-8"></div>
+                <div className="logo-circle circle-9"></div>
+            </div>
+            <div className="logo-line line-1"></div>
+            <div className="logo-line line-2"></div>
+            <div className="logo-line line-3"></div>
+        </div>
+    )
+}
+
+export default AlgoUILogo
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import AlgoUILogo from "../components/AlgoUILogo"
+
 const HomePage = () => {
     return (
         <div className="home-container">
             <h1>Home</h1>
+            <AlgoUILogo />
         </div>
     )
 }
